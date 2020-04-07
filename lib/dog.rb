d7226c61bb9ae7433f7edba5b7bf4b46605d7dc1
Rabiah Hogans <hogansrabiah@gmail.com>
2020-04-07 03:58:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,6 @@
 class Dog
   def name=(dog_name)
-    lassie = Dog.new
-lassie.name = "Lassie"
-lassie.name
+    
     this_dogs_name = dog_name
   end
  
@@ -13,3 +11,6 @@ lassie.name
   
 end
    
+   lassie = Dog.new
+lassie.name = "Lassie"
+lassie.name
